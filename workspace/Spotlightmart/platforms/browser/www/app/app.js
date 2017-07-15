78c7506a1e0c49be219a07cb66e682c897a9f1dc
Daniel Cheung <ios_developer@hotmail.com>
2017-07-15 00:04:07 -0700

    Picked a working swipe card carousel, still need to format the card markup

--- workspace/Spotlightmart/platforms/browser/www/app/app.js
+++ workspace/Spotlightmart/platforms/browser/www/app/app.js
@@ -35,7 +35,7 @@ angular.module('fsCordova', [])
   }]);
 
 
-var SpotlightmartApp = angular.module('SpotlightmartApp', ['fsCordova', 'ngRoute','ui.bootstrap','angular-cardflow','ngTouch']);
+var SpotlightmartApp = angular.module('SpotlightmartApp', ['fsCordova', 'ngRoute','ui.bootstrap','ngAnimate','ngTouch']);
 
 SpotlightmartApp.filter('substring', function() {
     return function(str, start, end) {
--- workspace/Spotlightmart/platforms/browser/www/app/home/homeCtrl.js
+++ workspace/Spotlightmart/platforms/browser/www/app/home/homeCtrl.js
@@ -1,10 +1,24 @@
 SpotlightmartApp.controller('homeCtrl', function ($scope, CordovaService, $location, $rootScope) {
     // generate 40 cards
+    $scope.currentIndex = 0;
     $scope.name = "John Doe";
-    $scope.cards = [];
-    var types = ['cats','fashion','nature','food','abstract','nightlife'];
-    for (var i=0; i<40; i++){
-        var t = types[i % types.length];
-        $scope.cards.push({image:'http://lorempixel.com/198/198/' + t + '/'+((i%10)+1), title: t});
-    }
+    $scope.slides = [
+        { quot : 'This is card #1'},
+        { quot : 'This is card #2'},
+        { quot : 'This is card #3'},
+        { quot : 'This is card #4'}
+    ];
+
+    $scope.prevSlide = function () {
+        $scope.direction = 'left';
+        $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+    };
+
+    $scope.nextSlide = function () {
+        $scope.direction = 'right';
+        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+    };    
+    $scope.isCurrentSlideIndex = function (index) {
+        return $scope.currentIndex === index;
+    };
 });
