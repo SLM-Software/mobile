c987faaa7afa22e8c74e10b086d9a921a62f3c86
Daniel Cheung <ios_developer@hotmail.com>
2017-08-09 22:57:25 -0700

    Added CordovaService.ready function to all controllers

--- workspace/Spotlightmart/www/app/controllers/homeCtrl.js
+++ workspace/Spotlightmart/www/app/controllers/homeCtrl.js
@@ -1,22 +1,24 @@
 SpotlightmartApp.controller('homeCtrl', function ($scope, CordovaService, $location, $rootScope) {
-    // generate 40 cards
-    $scope.currentIndex = 0;
-    $scope.name = "John Doe";
-    $scope.slides = [
-        { quot : '$1.00 Off Coke', img_url : '/img/Coke_20oz.png', msg : 'Buy 20oz Coke Get $1.00 Off' },
-        { quot : 'Free Cheerios', img_url : '/img/cheerios_en.png', msg : 'Buy 1 Get 1 Free' }
-    ];
+    CordovaService.ready.then(function () {
+        // generate 40 cards
+        $scope.currentIndex = 0;
+        $scope.name = "John Doe";
+        $scope.slides = [
+            { quot : '$1.00 Off Coke', img_url : '/img/Coke_20oz.png', msg : 'Buy 20oz Coke Get $1.00 Off' },
+            { quot : 'Free Cheerios', img_url : '/img/cheerios_en.png', msg : 'Buy 1 Get 1 Free' }
+        ];
 
-    $scope.prevSlide = function () {
-        $scope.direction = 'left';
-        $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-    };
+        $scope.prevSlide = function () {
+            $scope.direction = 'left';
+            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        };
 
-    $scope.nextSlide = function () {
-        $scope.direction = 'right';
-        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-    };    
-    $scope.isCurrentSlideIndex = function (index) {
-        return $scope.currentIndex === index;
-    };
-});
+        $scope.nextSlide = function () {
+            $scope.direction = 'right';
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+        };    
+        $scope.isCurrentSlideIndex = function (index) {
+            return $scope.currentIndex === index;
+        };
+    });
+}
