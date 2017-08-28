849479e2c74f70871637f1c0e1644c434be63a82
Daniel Cheung <ios_developer@hotmail.com>
2017-08-27 22:54:23 -0700

    Added Back button to /Settings/Personal/

--- workspace/Spotlightmart/www/app/controllers/settingsCtrl.js
+++ workspace/Spotlightmart/www/app/controllers/settingsCtrl.js
@@ -1,5 +1,10 @@
 SpotlightmartApp.controller('settingsCtrl', function ($scope, CordovaService, $location, $rootScope) {
     CordovaService.ready.then(function () {
+        init();
+        
+        function init() {
+        }
+        
         $scope.SettingClick = function() {
             $location.path("/Settings/Personal");
         }
