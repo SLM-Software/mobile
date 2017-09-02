840fc9531c4f7b2f37eff89550adc37c6c9b5a4c
Daniel Cheung <ios_developer@hotmail.com>
2017-09-02 01:22:35 -0700

    Added saving of image from photo album to cordova.file.dataDirectory folder

--- workspace/Spotlightmart/platforms/ios/www/app/controllers/mdlPersonalDetailCtrl.js
+++ workspace/Spotlightmart/platforms/ios/www/app/controllers/mdlPersonalDetailCtrl.js
@@ -36,12 +36,17 @@ SpotlightmartApp.controller('mdlPersonalDetailCtrl', function ($scope, CordovaSe
                 {
                     navigator.camera.getPicture(
                         function(rawData)
-                        {},
+                        {
+                            console.log("Image from camera : " + rawData);
+                        },
                         function(error)
-                        {},
+                        {
+                            alert("Photo error : " + error);
+                        },
                         { 
                             quality: 50,
-                            destinationType: Camera.DestinationType.DATA_URI,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            encodingType: Camera.EncodingType.JPEG,
                             sourceType: Camera.PictureSourceType.CAMERA
                         }
                     );
@@ -50,18 +55,57 @@ SpotlightmartApp.controller('mdlPersonalDetailCtrl', function ($scope, CordovaSe
                 else if (buttonIndex == 2)
                 {
                     navigator.camera.getPicture(
-                        function(rawData)
-                        {},
+                        function(imgFile)
+                        {
+                            console.log("Image from photo album : " + imgFile);
+                            movePic(imgFile);
+                        },
                         function(error)
-                        {},
+                        {
+                            alert("Photo album error : " + error);
+                        },
                         { 
                             quality: 50,
-                            destinationType: Camera.DestinationType.DATA_URI,
+                            destinationType: Camera.DestinationType.FILE_URI,
+                            encodingType: Camera.EncodingType.JPEG,
                             sourceType: Camera.PictureSourceType.PHOTOLIBRARY
                         }
                     );
                 }
             });
+            
+            function movePic(fileUri){ 
+                window.resolveLocalFileSystemURL(
+                    fileUri,
+                    function(fileEntry){
+                        newFileUri  = cordova.file.dataDirectory;
+                        oldFileUri  = fileUri;
+                        fileExt     = "." + oldFileUri.split('.').pop();
+
+                        newFileName = "portrait.jpg";
+                        window.resolveLocalFileSystemURL(newFileUri,
+                        function(dirEntry) {
+                            // move the file to a new directory and rename it
+                            fileEntry.moveTo(
+                                dirEntry, 
+                                newFileName, 
+                                function() {
+                                    console.log(cordova.file.dataDirectory + newFileName + " saved successfully");
+                                    $scope.input = cordova.file.dataDirectory + newFileName;
+                                    $scope.$apply();
+                                }, 
+                                errorCallback);
+                            },
+                            errorCallback);
+                    },
+                    errorCallback
+                );
+            }
+            
+            function errorCallback(error)
+            {
+                console.log("Error : %o", error);
+            }
         }
     });
 });
